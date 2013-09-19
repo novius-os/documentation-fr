5c80ebe7f1f1554259d40c9d0cc470c7ab71bf27
Gilles FELIX <felix@novius.com>
2013-09-19 18:05:33 +0200

    New theme Novius os

--- source/conf.py
+++ source/conf.py
@@ -98,19 +98,24 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'noviusos'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+	"sidebarwidth": 300,
+	"commonbar_lang" : "fr",
+	"commonbar_switch" : "en",
+	"commonbar_switch_url" : "http::docs.novius-os.org"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_theme']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = u'Documentation française'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -172,7 +177,6 @@ html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'NoviusOSdoc'
-
 
 # -- Options for LaTeX output --------------------------------------------------
 
